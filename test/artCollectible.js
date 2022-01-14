81e25c0cde4782b5240598a96199e97ce2be8057
abhalawat <97433422+abhalawat@users.noreply.github.com>
2022-01-14 12:40:30 +0530

    Update artCollectible.js

--- test/artCollectible.js
+++ test/artCollectible.js
@@ -8,6 +8,7 @@ const c = require('config');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+const baseURI = "https://ipfs.io/ipfs/" ;
 
 const artCollectible = artifacts.require('ArtCollectible');
 
@@ -39,12 +40,6 @@ contract('validator artcollectible contract()', (accounts) => {
     let err = null;
     let artCollectibleContract = await artCollectible.deployed();
     let errorSol = 'ERC721Metadata: URI query for nonexistent token';
-    //try {
-      //await artCollectibleContract.tokenURI(2);
-    //} catch (error) {
-      //err = error ;
-    //}
-    //expect(err).to.equal(errorSol);
     await expectRevert(
        artCollectibleContract.tokenURI(2),'ERC721Metadata: URI query for nonexistent token'
     );
@@ -55,7 +50,7 @@ contract('validator artcollectible contract()', (accounts) => {
     let artCollectibleContract = await artCollectible.deployed();
     const totalSupply = 1
     totalSupplySol = await artCollectibleContract.totalSupply()
-    expect(totalSupplySol.).to.equal(totalSupply)
+    expect(totalSupplySol.toNumber()).to.equal(totalSupply)
   });
 
   it('Should set base URI',async () =>{
@@ -72,6 +67,35 @@ contract('validator artcollectible contract()', (accounts) => {
   await truffleAssert.reverts(artCollectibleContract.setBaseURI('https;//anyone.com',{
     from: accounts[5]
   }),"Ownable: caller is not the owner")
+});
+
+it('token URI can be changed by changing the base URI', async function () {
+  let artCollectibleContract = await artCollectible.deployed();
+  const firstTokenId = new BN('1');
+  //await artCollectibleContract.setBaseURI(baseURI);
+  const newBaseURI = 'https://api.example.com/v2/';
+  await artCollectibleContract.setBaseURI(newBaseURI);
+  
+  expect(await artCollectibleContract.tokenURI(firstTokenId)).to.be.equal(newBaseURI + firstTokenId.toString());
+});
+
+it('base URI is added as a prefix to the token URI', async function () {
+  let artCollectibleContract = await artCollectible.deployed();
+  const firstTokenId = new BN('1');
+  await artCollectibleContract.setBaseURI(baseURI);
+  expect(await artCollectibleContract.tokenURI(firstTokenId)).to.be.equal(baseURI + firstTokenId.toString());
+});
+
+describe('metadata', function () {
+  it('has a name', async function () {
+    let artCollectibleContract = await artCollectible.deployed();
+    expect(await artCollectibleContract.name()).to.be.equal('artCollectible');
+  });
+
+  it('has a symbol', async function () {
+    let artCollectibleContract = await artCollectible.deployed();
+    expect(await artCollectibleContract.symbol()).to.be.equal('ARTC');
+  });
 });
 
 });
